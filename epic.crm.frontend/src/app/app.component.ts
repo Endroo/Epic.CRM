ef9ab1dee902636ec73a9ad139096a4b4aee23a8
stoth <toth.sandor@sbpsystems.hu>
2024-11-26 14:46:21 +0100

    footer,menu component

--- epic.crm.frontend/src/app/app.component.ts
+++ epic.crm.frontend/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'epic.crm.frontend';
+  title = 'Epic.CRM';
 }
--- epic.crm.frontend/src/app/app.module.ts
+++ epic.crm.frontend/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from 'src/material/material.module';
+import { MaterialModule } from 'src/app/material.module';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavMenuComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
