ba0366268a57f5a3d8eff803e1be18d0d7401bae
stoth <toth.sandor@sbpsystems.hu>
2024-11-26 14:06:01 +0100

    front-end base

---
+++ epic.crm.frontend/src/material/material.module.ts
@@ -0,0 +1,83 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
+import { MatPaginatorModule } from '@angular/material/paginator';
+
+@NgModule( {
+    imports: [
+        CommonModule,
+        MatCheckboxModule,
+        MatCheckboxModule,
+        MatButtonModule,
+        MatInputModule,
+        MatDatepickerModule,
+        MatFormFieldModule,
+        MatRadioModule,
+        MatSelectModule,
+        MatSliderModule,
+        MatSlideToggleModule,
+        MatMenuModule,
+        MatSidenavModule,
+        MatToolbarModule,
+        MatListModule,
+        MatCardModule,
+        MatTabsModule,
+        MatButtonToggleModule,
+        MatIconModule,
+        MatDialogModule,
+        MatTooltipModule,
+        MatTableModule,
+        MatSortModule,
+        MatPaginatorModule
+    ],
+    exports: [
+        MatCheckboxModule,
+        MatCheckboxModule,
+        MatButtonModule,
+        MatInputModule,
+        MatDatepickerModule,
+        MatFormFieldModule,
+        MatRadioModule,
+        MatSelectModule,
+        MatSliderModule,
+        MatSlideToggleModule,
+        MatMenuModule,
+        MatSidenavModule,
+        MatToolbarModule,
+        MatListModule,
+        MatCardModule,
+        MatTabsModule,
+        MatButtonToggleModule,
+        MatIconModule,
+        MatDialogModule,
+        MatTooltipModule,
+        MatTableModule,
+        MatSortModule,
+        MatPaginatorModule
+    ],
+    providers: [
+        MatDatepickerModule,
+    ]
+} )
+export class MaterialModule { }
