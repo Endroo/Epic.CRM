8506b9b4bac104614cebc62db02e9d8c7f5804be
Balázs Endre <balazs.endre@sbpsystems.hu>
2024-12-06 16:12:19 +0100

    DB + identity

--- epic.crm.backend/Epic.CRM.WebApi/Program.cs
+++ epic.crm.backend/Epic.CRM.WebApi/Program.cs
@@ -1,3 +1,8 @@
+using Epic.CRM.DataDomain;
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,22 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("EpicCrmConnection");
+builder.Services.AddDbContext<EpicCrmDbContext>(x => x.UseSqlServer(connectionString));
+
+builder.Services.AddAuthorization();
+builder.Services.AddIdentityApiEndpoints<IdentityUser>(opt =>
+{
+    opt.Password.RequiredLength = 8;
+    opt.User.RequireUniqueEmail = true;
+    opt.Password.RequireNonAlphanumeric = false;
+    opt.SignIn.RequireConfirmedEmail = true;
+})
+        .AddDefaultUI()
+        .AddEntityFrameworkStores<EpicCrmDbContext>();
+
+builder.Services.ConfigureApplicationCookie(options => { options.Cookie.SameSite = SameSiteMode.None; });
 
 var app = builder.Build();
 
@@ -16,9 +37,15 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.MapPost("/logout", async (SignInManager<IdentityUser> signInManager) =>
+{
+    await signInManager.SignOutAsync().ConfigureAwait(false);
+}).RequireAuthorization(); // So that only authorized users can use this endpoint
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+app.MapIdentityApi<IdentityUser>();
 
 app.MapControllers();
 
