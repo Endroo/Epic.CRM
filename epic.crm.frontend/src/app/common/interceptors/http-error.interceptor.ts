ed9a0f6968329391dff637e8d6d5d3e1ff5bd094
stoth <toth.sandor@sbpsystems.hu>
2025-01-09 17:50:26 +0100

    nyelvesites frontend - backendbol

--- epic.crm.frontend/src/app/common/interceptors/http-error.interceptor.ts
+++ epic.crm.frontend/src/app/common/interceptors/http-error.interceptor.ts
@@ -1,10 +1,9 @@
-import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { ToastrService } from 'ngx-toastr';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-import { Injectable } from '@angular/core';
-import { ToastrService } from 'ngx-toastr';
-import { TranslateService } from '@ngx-translate/core';
-import { Router } from '@angular/router';
 import { BadRequestError, BadRequestErrorParameters } from '../models/bad-request-error.model';
 
 @Injectable()
@@ -13,76 +12,37 @@ export class HttpErrorInterceptor implements HttpInterceptor {
   constructor(
     private toastrService: ToastrService,
     private translateService: TranslateService,
-    private router: Router
   ) { }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(catchError(error => {
-      if (error.error instanceof ErrorEvent) {
-        this.translateService.get('error.clientSide').subscribe((result: string) => {
+      if (error.status === 400) {
+        this.processValidationError(error);
+      } else if (error.status === 401) {
+        this.processValidationError(error);
+      }
+      else {
+        this.translateService.get(error.error).subscribe((result: string) => {
           this.toastrService.error(result);
         });
       }
-      else {
-        // Bad Request
-        if (error.status === 400) {
-          this.processValidationError(error);
-        }
-        // Unauthorized
-        else if (error.status === 401) {
-          this.translateService.get('error.unauthorized').subscribe((result: string) => {
-            this.toastrService.error(result);
-          });
-          this.router.navigate(['/']);
-        }
-        // Forbidden
-        else if (error.status === 403) {
-          this.router.navigate(['/']);
-        }
-        // Not Found
-        else if (error.status === 404) {
-          this.translateService.get('error.notFound').subscribe((result: string) => {
-            this.toastrService.error(result);
-          });
-        }
-        // Internal Server Error
-        else if (error.status === 500) {
-          this.translateService.get('error.internalServerError').subscribe((result: string) => {
-            this.toastrService.error(result);
-          });
-        }
-        // Service Unavailable
-        else if (error.status === 503) {
-          this.translateService.get('error.serviceUnavailable').subscribe((result: string) => {
-            this.toastrService.error(result);
-          });
-        }
-        else {
-          this.translateService.get('error.unspecified').subscribe((result: string) => {
-            this.toastrService.error(result);
-          });
-        }
-      }
-
       return throwError(error);
     }));
   }
 
   processValidationError(error: any): void {
-    const badRequestError: BadRequestError = error.error as BadRequestError;
+    const badRequestError: BadRequestError = error as BadRequestError;
 
-    if (badRequestError.errorLabel) {
+    if (badRequestError) {
       let translateParameters: any = {};
       if (badRequestError.error && badRequestError.error.length > 0) {
-        badRequestError.error.forEach((element: BadRequestErrorParameters) => {
-          translateParameters[element.key] = element.value;
-        });
+        translateParameters = badRequestError.error;
       }
 
       this.translateService
-        .get(badRequestError.errorLabel, translateParameters)
+        .get(translateParameters)
         .subscribe((result: string) => {
-    
+
           this.toastrService.error(result);
         });
     } else {
